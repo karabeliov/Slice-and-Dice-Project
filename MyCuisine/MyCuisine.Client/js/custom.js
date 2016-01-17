d445348665565e22a81c7731064fce8ef146621e
Kristian Karabeliov <kristian@karabeliov.com>
2016-01-17 13:40:13 +0200

    Add slider

---
+++ MyCuisine/MyCuisine.Client/js/custom.js
@@ -0,0 +1,15 @@
+﻿jQuery.noConflict();
+
+jQuery(document).ready(function(){
+	var $menu_right = jQuery('#menu-bg');
+
+	et_change_menu_width();
+
+	jQuery(window).resize( function(){
+		if ( ! et_idevice ) et_change_menu_width();
+	});
+
+	function et_change_menu_width(){
+		$menu_right.width( ( jQuery(window).width() - 970 ) * 3 );
+	}
+});
