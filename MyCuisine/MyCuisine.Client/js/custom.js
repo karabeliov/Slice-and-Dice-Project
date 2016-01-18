21e3f0588d37153a1858eb625fedcf5053e72067
Kristian Karabeliov <kristian@karabeliov.com>
2016-01-18 15:32:14 +0200

    Write style for menu page

--- MyCuisine/MyCuisine.Client/js/custom.js
+++ MyCuisine/MyCuisine.Client/js/custom.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
     var $menu_right = jQuery('#menu-bg');
 
 	function et_change_menu_width(){
-		$menu_right.width( ( jQuery(window).width() - 970 ) * 3 );
+		$menu_right.width( ( jQuery(window).width() - 986 ) * 3 );
 	}
 
 	et_change_menu_width();
