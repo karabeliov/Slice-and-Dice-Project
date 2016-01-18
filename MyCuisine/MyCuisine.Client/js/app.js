7d7372f7fa46dc5339c8a0b5f35134c2e16dc54e
Kristian Karabeliov <kristian@karabeliov.com>
2016-01-18 23:38:40 +0200

    add AngularJS and make single page application

---
+++ MyCuisine/MyCuisine.Client/js/app.js
@@ -0,0 +1,50 @@
+(function () {
+    'use strict';
+
+    function config($routeProvider, $locationProvider) {
+
+        var PARTIALS_PREFIX = 'views/';
+        var CONTROLLER_AS_VIEW_MODEL = 'vm';
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                title: 'Home',
+                style: 'home',
+                templateUrl: PARTIALS_PREFIX + 'home.html',
+                controller: 'HomeController',
+                controllerAs: CONTROLLER_AS_VIEW_MODEL
+            })
+            .when('/location', {
+                title: 'Location',
+                templateUrl: PARTIALS_PREFIX + 'location.html',
+                controllerAs: CONTROLLER_AS_VIEW_MODEL
+            })
+            .when('/menu', {
+                title: 'Menu',
+                templateUrl: PARTIALS_PREFIX + 'menu.html',
+                controllerAs: CONTROLLER_AS_VIEW_MODEL
+            })
+            .when('/blog', {
+                title: 'Blog',
+                templateUrl: PARTIALS_PREFIX + 'blog.html',
+                controllerAs: CONTROLLER_AS_VIEW_MODEL
+            })
+            .otherwise({ redirectTo: '/' });
+    }
+
+    angular.module('myApp.services', []);
+    angular.module('myApp.directives', []);
+    angular.module('myApp.controllers', ['myApp.services']);
+    var myApp = angular.module('myApp', ['ngRoute', 'myApp.controllers', 'myApp.directives']).
+        config(['$routeProvider', '$locationProvider', config])
+        .constant('baseServiceUrl', 'http://localhost:64352/');
+
+    myApp.run(['$location', '$rootScope', function ($location, $rootScope) {
+        $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+            $rootScope.title = current.$$route.title;
+        });
+        $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+            $rootScope.style = current.$$route.style || 'page';
+        });
+    }]);
+}());
---
+++ MyCuisine/MyCuisine.Client/js/controllers/home-controller.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    function HomeController() {
+        var vm = this;
+      
+    }
+
+    angular.module('myApp.controllers')
+        .controller('HomeController', [HomeController])
+}());
