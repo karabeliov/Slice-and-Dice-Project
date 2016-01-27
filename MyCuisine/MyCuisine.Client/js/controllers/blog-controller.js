a61fc2913dd1eebccbd148dbe5707d4dfd77e22b
Kristian Karabeliov <kristian@karabeliov.com>
2016-01-27 14:03:22 +0200

    fix comment bug and add loading screen

--- MyCuisine/MyCuisine.Client/js/controllers/blog-controller.js
+++ MyCuisine/MyCuisine.Client/js/controllers/blog-controller.js
@@ -4,6 +4,7 @@
     function BlogController($resource, $routeParams, $rootScope) {
         var vm = this;
         vm.orderProp = '-createdAt';
+        vm.loading = true;
         $rootScope.query = '';
         var limit = 3;
 
@@ -25,38 +26,14 @@
         .then(function (data) {
             var currentId = $routeParams.objectId;
             vm.posts = data.results;
-            //console.log(vm.posts);
             vm.currentPost = $.grep(data.results, function (e) { return e.objectId == currentId; })[0];
-            //console.log(vm.currentPost);
         })
         .catch(function (error) {
             console.log(error)
+        })
+        .finally(function () {
+            vm.loading = false;
         });
-
-
-        //COMMENTS
-        //var parseQueryComment = $resource('https://api.parse.com/1/classes/Comment', {}, {
-        //    getCom: {
-        //        method: 'GET',
-        //        headers: {
-        //            'X-Parse-Application-Id': 'BtESBJZiztQr2rsfiyrhJT0BhA26EL8CmnNWamvS',
-        //            'X-Parse-REST-API-Key': '8DbU4OmT5kuqPP6S8UlOdVur2m5KcgXcJ8sMK2Zz',
-        //        },
-        //        params: {
-        //            limit: limit,
-        //            order: vm.orderProp
-        //        }
-        //    }
-        //});
-        //parseQueryComment.getCom().$promise
-        //.then(function (data) {
-        //    var currentId = $routeParams.objectId;
-        //    vm.comments = data.results;
-        //    //console.log(vm.comments);
-        //})
-        //.catch(function (error) {
-        //    console.log(error)
-        //});
     }
 
     angular.module('myApp.controllers')
