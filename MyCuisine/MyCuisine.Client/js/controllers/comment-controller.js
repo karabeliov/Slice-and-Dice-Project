12e2752bbb304b015cf7b9aa1072c47aaa528a76
Kristian Karabeliov <kristian@karabeliov.com>
2016-01-31 13:52:12 +0200

    now it is possibly to reply in comment

--- MyCuisine/MyCuisine.Client/js/controllers/comment-controller.js
+++ MyCuisine/MyCuisine.Client/js/controllers/comment-controller.js
@@ -16,10 +16,39 @@
                 takePostQuery.first({
                     success: function (post) {
                         comment.date = post.updatedAt;
+                        comment.reply = [];
                         comment.sender = currentUser.get('fname') || currentUser.get('username');
                         post.add('Comments', comment);
                         post.save();
                         notifier.success('Comment is add!');
+                        $window.location.assign('/post/' + postId);
+                    },
+                    error: function (error) {
+                        notifier.error("Error: " + error.code + " " + error.message);
+                    }
+                });
+            } else {
+                notifier.error("You not login!");
+                $location.path('/login');
+            }
+        }
+
+        // ADD Reply to Current COMMENT
+        vm.replyComment = function (reply, postId, commentId) {
+            console.log(reply, postId, commentId);
+            var currentUser = Parse.User.current();
+            if (currentUser) {
+                reply.sender = currentUser.get('fname') || currentUser.get('username');
+
+                var Post = Parse.Object.extend("Post");
+                var takePostQuery = new Parse.Query(Post);
+                takePostQuery.equalTo("objectId", postId);
+                takePostQuery.first({
+                    success: function (post) {
+                        var comments = post.get('Comments');
+                        comments[commentId].reply.push(reply);
+                        post.save();
+                        notifier.success('Reply is add!');
                         $window.location.assign('/post/' + postId);
                     },
                     error: function (error) {
